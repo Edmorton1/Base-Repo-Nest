433339c456c3668d7630e9e755f44f502a62a5af
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-08-06 13:35:05 +0500

    Add tests

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@libs/types": path.resolve(__dirname, "../../libs/types/src"),
+			"@libs/shared": path.resolve(__dirname, "../../libs/shared/src"),
+			"@apps/client": path.resolve(__dirname, "./src"),
+		},
+	},
 });
-
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,10 +8,11 @@ import { defineConfig } from "eslint/config";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactRefresh from "eslint-plugin-react-refresh";
+import pluginCypress from "eslint-plugin-cypress";
 
 export default defineConfig([
 	{
-		ignores: ["node_modules/", "dist/"],
+		ignores: ["node_modules/", "dist/", "eslint.config.mjs"],
 	},
 	js.configs.recommended,
 	tseslint.configs.recommended,
@@ -20,16 +21,18 @@ export default defineConfig([
 	jsxA11y.flatConfigs.recommended,
 	eslintPluginPrettierRecommended,
 	reactRefresh.configs.vite,
+	pluginCypress.configs.recommended,
 	{
 		files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
 		languageOptions: {
 			ecmaVersion: "latest",
 			globals: { ...globals.browser, ...globals.node, ...globals.jest },
 			parserOptions: {
-				project: "./tsconfig.base.json",
+				project: "./tsconfig.json",
 				tsconfigRootDir: import.meta.dirname,
 			},
 		},
+		// REACT
 		plugins: {
 			react: pluginReact,
 		},
@@ -39,10 +42,11 @@ export default defineConfig([
 			},
 		},
 		rules: {
+			// REACT
 			"react/react-in-jsx-scope": "off",
 			"react-hooks/exhaustive-deps": "error",
 			"react/display-name": "off",
-			
+
 			"@typescript-eslint/no-explicit-any": "off",
 			"@typescript-eslint/no-floating-promises": "warn",
 			"@typescript-eslint/no-unsafe-argument": "warn",
