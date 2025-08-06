a4d0f9ab34d1f40ca354af9f921d1a6e649d8e3c
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-08-06 19:06:21 +0500

    test husky push

--- apps/client/src/testing/unit/function.spec.ts
+++ apps/client/src/testing/unit/function.spec.ts
@@ -3,6 +3,6 @@ import { calc } from "@apps/client/testing/unit/function";
 describe("[FUNCTION TESTING]", () => {
 	it("should be 4", () => {
 		const result = calc(2, 2);
-		expect(result).toStrictEqual(4);
+		expect(result).toStrictEqual(5);
 	});
 });
